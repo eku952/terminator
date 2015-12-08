dde8b3517e2623d682c8e69a007fd15b9ce9c193
Richik SC <arko03@yahoo.com>
2015-12-07 22:12:07 -0600

    Solving the rebase problem, this should do it

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/customcode/AutonomousBlue.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/customcode/AutonomousBlue.java
@@ -11,9 +11,6 @@ package com.qualcomm.ftcrobotcontroller.opmodes.customcode;
 
 public class AutonomousBlue extends ETAutonomousBase {
 
-  private static double lCounts;
-  private static double rCounts;
-
   @Override
   public void etLoop() throws InterruptedException {
 
@@ -23,32 +20,20 @@ public class AutonomousBlue extends ETAutonomousBase {
       case STAGE_MOVE_FIRST:
       {
         counts = getCountsForDistance(MOVE_FIRST_DISTANCE);
-        telemetry.addData("STAGE", "STAGE_MOVE_FIRST");
-        if(hasEncoderReset()) {
-          encoderDrive(MOVE_POWER, MOVE_POWER, counts, counts);
-          stage = STAGE_CONTINUE_MOVE_FIRST;
-        }
-        break;
-      }
+        telemetry.addData("stage 20", counts);
 
-      case STAGE_CONTINUE_MOVE_FIRST: {
-        if (encoderDrive(MOVE_POWER, MOVE_POWER, counts, counts)) {
+        setTarget(MOVE_POWER);
+        if(hasArrived(counts))
           stage = STAGE_REVERSE_FIRST;
-        }
         break;
       }
 
       case STAGE_REVERSE_FIRST: {
+        // goes -6 dst.
         counts = getCountsForDistance(REVERSE_FIRST_DISTANCE);
-        if(hasEncoderReset()) {
-          encoderDrive(MOVE_POWER * -1, MOVE_POWER * -1, counts, counts);
-          stage = STAGE_CONTINUE_REVERSE_FIRST;
-        }
-        break;
-      }
-
-      case STAGE_CONTINUE_REVERSE_FIRST: {
-        if(encoderDrive(MOVE_POWER * -1, MOVE_POWER * -1, counts, counts)) {
+        setTarget(MOVE_POWER * -1);
+        if(hasArrived(counts)) {
+          telemetry.addData("hasArrived Reverse", "true");
           stage = STAGE_TURN_FIRST;
         }
         break;
@@ -56,52 +41,23 @@ public class AutonomousBlue extends ETAutonomousBase {
 
       case STAGE_TURN_FIRST: {
         // turns right
-        lCounts = getCountsForDistance(TURN_COUNTS);
-        rCounts = lCounts * -1;
-        if(hasEncoderReset()) {
-          encoderDrive(MOVE_POWER, MOVE_POWER * -1, lCounts, rCounts);
-          stage = STAGE_TURN_FIRST;
-        }
+        turn(-0.25, 0.25);
+        stage = STAGE_MOVE_SECOND;
         break;
       }
 
-      case STAGE_CONTINUE_TURN_FIRST: {
-        if(encoderDrive(MOVE_POWER, MOVE_POWER * -1, lCounts, rCounts)) {
-          pusherUp();
-          stage = STAGE_MOVE_SECOND;
-        }
-        break;
-      }
-
-      case STAGE_MOVE_SECOND: {
+      case STAGE_MOVE_SECOND : {
         counts = getCountsForDistance(MOVE_SECOND_DISTANCE);
-        if(hasEncoderReset()) {
-          encoderDrive(MOVE_POWER, MOVE_POWER, counts, counts);
-          stage = STAGE_CONTINUE_MOVE_SECOND;
-        }
-        break;
-      }
-
-      case STAGE_CONTINUE_MOVE_SECOND: {
-        if(encoderDrive(MOVE_POWER, MOVE_POWER, counts, counts)) {
+        setTarget(MOVE_POWER);
+        if(hasArrived(counts))
           stage = STAGE_STOP;
-        }
         break;
       }
 
       case STAGE_STOP: {
         stopRobot();
-        stage = STAGE_BREAK_LOOP;
         break;
       }
-
-      case STAGE_BREAK_LOOP: {
-        if(hasEncoderReset()) {
-          etBreakLoop();
-        }
-        break;
-      }
-
     }
   }
 }
--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/customcode/AutonomousRed.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/customcode/AutonomousRed.java
@@ -11,98 +11,53 @@ package com.qualcomm.ftcrobotcontroller.opmodes.customcode;
 
 public class AutonomousRed extends ETAutonomousBase {
 
-  private static double lCounts;
-  private static double rCounts;
-
   @Override
   public void etLoop() throws InterruptedException {
 
-    telemetry.addData("STAGE", stage);
+    telemetry.addData("stage", stage);
 
     switch (stage) {
       case STAGE_MOVE_FIRST:
       {
         counts = getCountsForDistance(MOVE_FIRST_DISTANCE);
-        telemetry.addData("STAGE", "STAGE_MOVE_FIRST");
-        if(hasEncoderReset()) {
-          encoderDrive(MOVE_POWER, MOVE_POWER, counts, counts);
-          stage = STAGE_CONTINUE_MOVE_FIRST;
-        }
-        break;
-      }
+        telemetry.addData("stage 20", counts);
 
-      case STAGE_CONTINUE_MOVE_FIRST: {
-        if (encoderDrive(MOVE_POWER, MOVE_POWER, counts, counts)) {
+        setTarget(MOVE_POWER);
+        if(hasArrived(counts))
           stage = STAGE_REVERSE_FIRST;
-        }
         break;
       }
 
       case STAGE_REVERSE_FIRST: {
+        // goes -6 dst.
         counts = getCountsForDistance(REVERSE_FIRST_DISTANCE);
-        if(hasEncoderReset()) {
-          encoderDrive(MOVE_POWER * -1, MOVE_POWER * -1, counts, counts);
-          stage = STAGE_CONTINUE_REVERSE_FIRST;
-        }
-        break;
-      }
-
-      case STAGE_CONTINUE_REVERSE_FIRST: {
-        if(encoderDrive(MOVE_POWER * -1, MOVE_POWER * -1, counts, counts)) {
+        setTarget(MOVE_POWER * -1);
+        if(hasArrived(counts)) {
+          telemetry.addData("hasArrived Reverse", "true");
           stage = STAGE_TURN_FIRST;
         }
         break;
       }
 
       case STAGE_TURN_FIRST: {
-        // turns left
-        rCounts = getCountsForDistance(TURN_COUNTS);
-        lCounts = rCounts * -1;
-
-        if(hasEncoderReset()) {
-          encoderDrive(MOVE_POWER, MOVE_POWER * -1, lCounts, rCounts);
-          stage = STAGE_TURN_FIRST;
-        }
+        // turns right
+        turn(0.25, -0.25);
+        stage = STAGE_MOVE_SECOND;
         break;
       }
 
-      case STAGE_CONTINUE_TURN_FIRST: {
-        if(encoderDrive(MOVE_POWER, MOVE_POWER * -1, lCounts, rCounts)) {
-          pusherUp();
-          stage = STAGE_MOVE_SECOND;
-        }
-        break;
-      }
-
-      case STAGE_MOVE_SECOND: {
+      case STAGE_MOVE_SECOND : {
         counts = getCountsForDistance(MOVE_SECOND_DISTANCE);
-        if(hasEncoderReset()) {
-          encoderDrive(MOVE_POWER, MOVE_POWER, counts, counts);
-          stage = STAGE_CONTINUE_MOVE_SECOND;
-        }
-        break;
-      }
-
-      case STAGE_CONTINUE_MOVE_SECOND: {
-        if(encoderDrive(MOVE_POWER, MOVE_POWER, counts, counts)) {
+        setTarget(MOVE_POWER);
+        if(hasArrived(counts))
           stage = STAGE_STOP;
-        }
         break;
       }
 
       case STAGE_STOP: {
         stopRobot();
-        stage = STAGE_BREAK_LOOP;
         break;
       }
-
-      case STAGE_BREAK_LOOP: {
-        if(hasEncoderReset()) {
-          etBreakLoop();
-        }
-        break;
-      }
-
     }
   }
 }
